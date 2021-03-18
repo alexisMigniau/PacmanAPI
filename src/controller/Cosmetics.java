f57d809280877e3bf90cdb53bca435ba6bc655c8
C_NNS <nunes.clement19@gmail.com>
2021-03-18 23:16:19 +0100

    [Cosmetic] Début de DAO & class

---
+++ src/controller/Cosmetics.java
@@ -0,0 +1,55 @@
+package controller;
+
+import java.io.IOException;
+
+import dao.DAOFactory;
+import dao.CosmeticDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/cosmetics")
+public class Cosmetics extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private CosmeticDao cosmeticDao;
+	
+	public void init() throws ServletException
+	{
+		this.cosmeticDao = ((DAOFactory) getServletContext().getAttribute("daofactory")).getCosmeticDao();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(cosmeticDao.findById((long) 1));
+		this.getServletContext().getRequestDispatcher("/WEB-INF/formCosmetic.jsp").forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/dao/CosmeticDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.util.List;
+
+import bean.Cosmetic;
+
+public interface CosmeticDao {
+
+		// Création d'une cosmetique
+		void ajouter(Cosmetic cosmetic) throws DAOException;
+		
+		// Trouver cosmetique par id
+		Cosmetic findById(Long id) throws DAOException;
+		
+		// Trouver cosmetique par nom
+		Cosmetic findByName(String name) throws DAOException;
+
+		// Modifier un joueurformPlayer
+		boolean update(long id, Cosmetic cosmetic) throws DAOException;
+		
+		// Supprimer un joueur
+		boolean delete(long id) throws DAOException;
+	}
+
