1dd75144a3a75a06f7b223fb6cc6287d714522dc
AlexisMigniau <migniau.alexis@gmail.com>
2021-04-02 12:28:17 +0200

    Changement de méthode pour appel de connexion depuis l'API REST

--- src/controller/PlayerConnexion.java
+++ src/controller/PlayerConnexion.java
@@ -31,47 +31,18 @@ public class PlayerConnexion extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		this.getServletContext().getRequestDispatcher("/WEB-INF/formConnexionPlayer.jsp").forward( request, response );
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// Si le paramètre Token est présent, ça veut dire que c'est le serveur qui interroge donc on retourne sous forme de JSON et non en vue HTML
+		// Si il y a un token alors on retourne au format JSON
 		String token = request.getParameter("token");
 		
-		
-		// Création du formulaire
-		PlayerForm form = new PlayerForm();
-		
-		Player player = form.connexionPlayer(request);
-		
-		player = playerDao.findByLogin(player.getLogin(), player.getPassword());
-		
 		if(token == null)
-		{
-			// Si on ne trouve pas de joueur alors on renvoie une erreur, et on renvoie sur la page de connexion
-			if(player == null)
-			{
-				request.setAttribute("erreur", true);
-				this.getServletContext().getRequestDispatcher("/WEB-INF/formConnexionPlayer.jsp").forward( request, response );
-			}
-			// Si on trouve un joueur, alors on définit les variables de session, et on renvoie sur la page d'accueil
-			else {
-				player.setPassword("");
-				HttpSession session = request.getSession();
-				session.setAttribute("player", player);
-				
-				response.sendRedirect(request.getContextPath() + "/home");
-			}
+			this.getServletContext().getRequestDispatcher("/WEB-INF/formConnexionPlayer.jsp").forward( request, response );
+		else if(token.equals("8RCrv0rBNjpPPtTXvOTV")){
 			
-		} 
-		// Si le token est valide alors on renvoie le résultat sous forme de JSON
-		else if(token.equals("8RCrv0rBNjpPPtTXvOTV"))
-		{
+			// Création du formulaire
+			PlayerForm form = new PlayerForm();
+			Player player = form.connexionPlayer(request);
+			player = playerDao.findByLogin(player.getLogin(), player.getPassword());
+			
 			boolean resultat = true;
 			String message = "Connexion réussie";
 			
@@ -93,6 +64,33 @@ public class PlayerConnexion extends HttpServlet {
 			
 			this.getServletContext().getRequestDispatcher("/WEB-INF/JSON/ConnexionPlayer.jsp").forward( request, response );
 		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Création du formulaire
+		PlayerForm form = new PlayerForm();
+		Player player = form.connexionPlayer(request);
+		player = playerDao.findByLogin(player.getLogin(), player.getPassword());
+		
+		// Si on ne trouve pas de joueur alors on renvoie une erreur, et on renvoie sur la page de connexion
+		if(player == null)
+		{
+			request.setAttribute("erreur", true);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/formConnexionPlayer.jsp").forward( request, response );
+		}
+		// Si on trouve un joueur, alors on définit les variables de session, et on renvoie sur la page d'accueil
+		else {
+			player.setPassword("");
+			HttpSession session = request.getSession();
+			session.setAttribute("player", player);
+			
+			response.sendRedirect(request.getContextPath() + "/home");
+		}
+			
 	}
 
 }
